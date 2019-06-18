a725bb75706370ddde119a02621e2341954ed992
Inzamam <iaminzmam@gmail.com>
2019-06-18 13:02:07 +0530

    commit whole repo

---
+++ search/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'banks.html', {})
+
+def bankdetails(request):
+    id = request.GET.get('id')
+    name = request.GET.get('name')
+    ifsc = request.GET.get('ifsc')
+    branch = request.GET.get('branch')
+    city = request.GET.get('city')
+    address = request.GET.get('address')
+    disrict = request.GET.get('dist')
+    state = request.GET.get('state')
+    context = {'id': id, 'name': name, 'ifsc': ifsc, 'branch': branch, 'city':city, 'address': address, 'dist': disrict, 'state': state}
+    return render(request, 'bankdetail.html', context)
+
+def bank(request, bank_id):
+    return HttpResponse("<h3>your bank id is: " + bank_id)
